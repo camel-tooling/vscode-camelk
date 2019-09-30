f66d5ab3696f93777e58d6452cd4da96219ae9f3
Brian Fitzpatrick <bfitzpat@redhat.com>
2019-09-30 16:23:14 -0600

    issue #50 - adding tests and comment headers where missing
    -- added install test to ensure cli files are available after activation and install
    -- added cli test to ensure that both kamel and kubectl are executable as well
    -- addressed some feedback from code review
    -- fixed issue in kamelInternal and kubectlInternal where newline at end of file path was causing problems
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

---
+++ src/test/install.test.ts
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import * as config from '../config';
+import * as fs from 'fs';
+
+suite("ensure install methods are functioning as expected", function() {
+
+	test("install Camel K CLI on activation", function(done) {
+		const extensionId = 'redhat.vscode-camelk';
+		let extension = vscode.extensions.getExtension(extensionId);
+		if (extension !== null && extension !== undefined) {
+			extension.activate().then(() => {
+				if (extension !== null && extension !== undefined) {
+					let kamelPath = config.getActiveKamelconfig();
+					assert.equal(fs.existsSync(kamelPath), true);
+				}
+			});
+		}
+		done();
+	});
+
+	test("install Kubernetes CLI on activation", function(done) {
+		const vskubernetesextensionId = 'vscode-kubernetes-tools';
+		let extension = vscode.extensions.getExtension(vskubernetesextensionId);
+		if (extension !== null && extension !== undefined) {
+			extension.activate().then(() => {
+				if (extension !== null && extension !== undefined) {
+					let kubectlPath = config.getActiveKubectlconfig();
+					assert.equal(fs.existsSync(kubectlPath), true);
+				}
+			});
+		}
+		done();
+	});
+
+});
