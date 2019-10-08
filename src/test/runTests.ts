3008dc5dbc9c7029c7865ce6de5550a5cdc831e0
Aurélien Pupier <apupier@redhat.com>
2019-10-08 09:48:36 +0200

    Install Kubernetes extension before playing tests #50
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -1,75 +1,31 @@
 import * as path from 'path';
 import * as cp from 'child_process';
 
-const { downloadAndUnzipVSCode, runTests } = require('vscode-test');
+import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from 'vscode-test';
 
-export function resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath: string) {
-	if (process.platform === 'win32') {
-		if (vscodeExecutablePath.endsWith('Code - Insiders.exe')) {
-			return path.resolve(vscodeExecutablePath, '../bin/code-insiders.cmd');
-		} else {
-			return path.resolve(vscodeExecutablePath, '../bin/code.cmd');
-		}
-	} else if (process.platform === 'darwin') {
-		return path.resolve(vscodeExecutablePath, '../../../Contents/Resources/app/bin/code');
-	} else if (process.platform === 'linux') {
-		if (vscodeExecutablePath.endsWith('code-insiders')) {
-			return path.resolve(vscodeExecutablePath, '../code-insiders');
-		} else {
-			return path.resolve(vscodeExecutablePath, '../code');
-		}
-	} else {
-		if (vscodeExecutablePath.endsWith('code-insiders')) {
-			return path.resolve(vscodeExecutablePath, '../bin/code-insiders');
-		} else {
-			return path.resolve(vscodeExecutablePath, '../bin/code');
-		}
-	}
-}
-
-async function doTheThing(vscodePath : string ) : Promise<string> {
-	return new Promise<string>( async (resolve, reject) => {
-		/**
-		 * Install Kubernetes extension
-		 */
-        const extensionId : string = 'ms-kubernetes-tools.vscode-kubernetes-tools';
-		const cliPath : string = resolveCliPathFromVSCodeExecutablePath(vscodePath);
-		console.log(`cliPath = ${cliPath}`);
-		const spawnMe = cp.spawn(cliPath, ['--install-extension', '--force', extensionId]);
-		if (spawnMe.stderr) {
-			spawnMe.stderr.on('data', function (data) {
-				console.log(`Error ${data}`);
-				reject(data);
-				return;
-			});
-		}
-		resolve(cliPath);
-		return;
-	});
-}
-
-async function go() : Promise<void> {
+async function main() : Promise<void> {
 	try {
-		const version = '1.36.1'; // 'stable'
 		const extensionDevelopmentPath : string = path.resolve(__dirname, '../../');
 		console.log(`extensionDevelopmentPath = ${extensionDevelopmentPath}`);
         const extensionTestsPath : string = path.resolve(__dirname, './suite/index');
 		console.log(`extensionTestsPath = ${extensionTestsPath}`);
-		const vscodeExecutablePath : string = await downloadAndUnzipVSCode(version);
+		const vscodeExecutablePath : string = await downloadAndUnzipVSCode('stable');
 		console.log(`vscodeExecutablePath = ${vscodeExecutablePath}`);
 
-		/**
-		 * Install Kubernetes extension
-		 */
-		const cliPath : any = doTheThing(vscodeExecutablePath);
-		if (cliPath) {
-			console.log(`Kubernetes VS Code extension installed`);
-			await runTests({ vscodeExecutablePath, version, extensionDevelopmentPath, extensionTestsPath });
-		}
+		const cliPath: string = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+		cp.spawnSync(cliPath, ['--install-extension', 'ms-kubernetes-tools.vscode-kubernetes-tools'],
+		{
+			encoding: 'utf-8',
+			stdio: 'inherit'
+		});
+		console.log(`Kubernetes VS Code extension installed`);
+		
+		await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+
 	} catch (err) {
-		console.error('Failed to run tests');
+		console.error('Failed to run tests' + err);
 		process.exit(1);
 	}
 }
 
-go();
+main();
