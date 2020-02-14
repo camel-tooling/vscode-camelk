a662f55254b945c492b565ae520d01959b44d299
Aurélien Pupier <apupier@redhat.com>
2020-02-14 15:33:06 +0100

    FUSETOOLS2-226 - Configure code coverage
    inspired from vscode-tekton configuration
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -10,21 +10,44 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
+import * as fs from 'fs';
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import { TestRunnerOptions, CoverageRunner } from './../coverage';
+
+// Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
+// Since we are not running in a tty environment, we just implement the method statically
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tty = require('tty');
+if (!tty.getWindowSize) {
+    tty.getWindowSize = (): number[] => {
+        return [80, 75];
+    };
+}
+
+function loadCoverageRunner(testsRoot: string): CoverageRunner | undefined {
+	let coverageRunner: CoverageRunner;
+	const coverConfigPath = path.join(testsRoot, '..', '..', '..', 'coverconfig.json');
+	if (!process.env.VST_DISABLE_COVERAGE && fs.existsSync(coverConfigPath)) {
+		coverageRunner = new CoverageRunner(JSON.parse(fs.readFileSync(coverConfigPath, 'utf-8')) as TestRunnerOptions, testsRoot);
+		coverageRunner.setupCoverage();
+		return coverageRunner;
+	}
+}
+
+let mocha = new Mocha({
+	ui: 'tdd',
+	timeout: 20000,
+	reporter: 'mocha-jenkins-reporter',
+	useColors: true
+});
 
 export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		timeout: 20000,
-		reporter: 'mocha-jenkins-reporter',
-		useColors: true
-	});
-	
+
 	const testsRoot = path.resolve(__dirname, '..');
 	console.log(`testsRoot = ${testsRoot}`);
+	const coverageRunner = loadCoverageRunner(testsRoot);
 
 	return new Promise((c, e) => {
 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
@@ -45,7 +68,7 @@ export function run(): Promise<void> {
 					} else {
 						c();
 					}
-				});
+				}).on('end', () => coverageRunner && coverageRunner.reportCoverage());
 			} catch (err) {
 				e(err);
 			}
