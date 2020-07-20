9f46a624497f73e027e0a4c3de9d8d7a7bf2b653
Lars Heinemann <lhein.smx@gmail.com>
2020-07-20 17:36:04 +0200

    refactored installer.ts
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/installer.ts
+++ src/installer.ts
@@ -17,8 +17,6 @@
 
 import * as path from 'path';
 import * as fs from 'fs';
-import mkdirp = require('mkdirp');
-import {platform} from 'os';
 import { Errorable, failed } from './errorable';
 import * as extension from './extension';
 import * as config from './config';
@@ -29,91 +27,53 @@ import * as kubectlutils from './kubectlutils';
 import * as downloader from './downloader';
 import * as download from 'download';
 import * as versionUtils from './versionUtils';
+import * as mkdirp from 'mkdirp';
 
 export const kamel = 'kamel';
 export const kamel_windows = 'kamel.exe';
-export const PLATFORM_WINDOWS = 'windows';
-export const PLATFORM_MAC = 'mac';
-export const PLATFORM_LINUX = 'linux';
+export const platformString = shell.getPlatform(); // looks for windows, mac, linux
+const binFile: string = (!shell.isWindows()) ? kamel : kamel_windows;
 
-export const platformString = getPlatform(); // looks for windows, mac, linux
-const isWindows = (platformString === 'windows');
-const binFile = (!isWindows) ? kamel : kamel_windows;
-
-export function isKamelAvailable() : Promise<boolean> {
-	return new Promise<boolean>( async (resolve, reject) => {
+export async function isKamelAvailable() : Promise<boolean> {
+	try { 
 		let kamelLocal = kamelCli.create();
-		await kamelLocal.invoke('--help')
-			.then( (rtnValue) => {
-				if (rtnValue.startsWith('Apache Camel K')) {
-					resolve(true);
-					return;
-				} else {
-					resolve(false);
-					return;
-				}
-			}).catch (kamelUnavailableRejection(reject));
-	});
+		const rtnValue: string = await kamelLocal.invoke('--help');
+		return rtnValue.startsWith('Apache Camel K');
+	} catch (error) {
+		Promise.reject(error);
+		return false;
+	}
 }
 
-export function kamelUnavailableRejection(reject: (reason?: any) => void): ((reason: any) => void | PromiseLike<void>) | null | undefined {
-	return (error) => {
-		console.log(`Apache Camel K CLI (kamel) unavailable: ${error}`);
-		reject(new Error(error));
-		return;
-	};
-}
-
-export function isKubernetesAvailable(): Promise<boolean> {
-	return new Promise<boolean>( async (resolve, reject) => {
-		const version = await kubectlutils.getKubernetesVersion();
-		if (version) {
-			resolve(true);
-			return;
-		}
-		reject (new Error('No kubectl version found'));
-		return;
-	});
-}
-
-export function getPlatform() : string | undefined {
-	const os = platform();
-	const isWindows = (os === 'win32');
-	const isMac = (os === 'darwin');
-	const isLinux = (os === 'linux');
-
-	if (isWindows) { return PLATFORM_WINDOWS; }
-	if (isMac) { return PLATFORM_MAC; }
-	if (isLinux) { return PLATFORM_LINUX; }
-	return undefined;
+export async function isKubernetesAvailable(): Promise<boolean> {
+	return await kubectlutils.getKubernetesVersion() !== undefined;
 }
 
 async function downloadAndExtract(link : string, dlFilename: string, installFolder : string, extractFlag : boolean) : Promise<boolean> {
-	let myStatusBarItem: vscode.StatusBarItem;
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-
-	const downloadSettings = {
+	const myStatusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	const downloadSettings: any = {
 		filename: `${dlFilename}`,
 		extract: extractFlag,
-	  };
+	};
 	extension.mainOutputChannel.appendLine('Downloading from: ' + link);
 	await download(link, installFolder, downloadSettings)
 		.on('response', (response) => {
 			extension.mainOutputChannel.appendLine(`Bytes to transfer: ${response.headers['content-length']}`);
 		}).on('downloadProgress', (progress) => {
-			let incr = progress.total > 0 ? Math.floor(progress.transferred / progress.total * 100) : 0;
-			let percent = Math.round(incr);
-			let message = `Download progress: ${progress.transferred} / ${progress.total} (${percent}%)`;
-			let tooltip = `Download progress for ${dlFilename}`;
+			const incr: number = progress.total > 0 ? Math.floor(progress.transferred / progress.total * 100) : 0;
+			const percent: number = Math.round(incr);
+			const message: string = `Download progress: ${progress.transferred} / ${progress.total} (${percent}%)`;
+			const tooltip: string = `Download progress for ${dlFilename}`;
 			updateStatusBarItem(myStatusBarItem, message, tooltip);
-		}).then(async () => {
+		}).then(() => {
 			extension.mainOutputChannel.appendLine(`Downloaded ${dlFilename}.`);
 			myStatusBarItem.dispose();
 			return true;
 		}).catch((error) => {
 			console.log(error);
+		}).finally( () => {
+			myStatusBarItem.dispose();
 		});
-	myStatusBarItem.dispose();
 	return false;
 }
 
@@ -129,11 +89,11 @@ function updateStatusBarItem(sbItem : vscode.StatusBarItem, text: string, toolti
 
 export async function installKamel(context: vscode.ExtensionContext): Promise<Errorable<null>> {
 	let versionToUse: string;
-	const runtimeVersionSetting = vscode.workspace.getConfiguration().get(config.RUNTIME_VERSION_KEY) as string;
+	const runtimeVersionSetting: string | undefined = vscode.workspace.getConfiguration().get(config.RUNTIME_VERSION_KEY);
 	if (runtimeVersionSetting && runtimeVersionSetting.toLowerCase() !== versionUtils.version.toLowerCase()) {
 		versionToUse = runtimeVersionSetting;
 	} else {
-		const latestversion = await versionUtils.getLatestCamelKVersion();
+		const latestversion: Errorable<string> = await versionUtils.getLatestCamelKVersion();
 		if (failed(latestversion)) {
 			extension.shareMessageInMainOutputChannel(`Cannot retrieve latest available Camel version and none has been specified in settings. Will fall back to use the default ${versionUtils.version}`);
 			versionToUse = versionUtils.version;
@@ -142,35 +102,35 @@ export async function installKamel(context: vscode.ExtensionContext): Promise<Er
 		}
 	}
 
-	await versionUtils.checkKamelNeedsUpdate(versionToUse).then((needsUpdate) => {
+	try {
+		const needsUpdate: boolean = await versionUtils.checkKamelNeedsUpdate(versionToUse);
 		if (needsUpdate) {
 			extension.shareMessageInMainOutputChannel(`Checking to see if Apache Camel K CLI version ${versionToUse} available`);
 			return { succeeded: true, result: null };
 		}
-	}).catch ( (error) => {
+	} catch ( error ) {
 		console.error(error);
-	});
+	}
 
-	const installFolder = getInstallFolder(kamel, context);
+	const installFolder: string = getInstallFolder(kamel, context);
 	console.log(`Attempting to download Apache Camel K CLI to ${installFolder}`);
 	mkdirp.sync(installFolder);
 
-	const kamelUrl = `https://github.com/apache/camel-k/releases/download/${versionToUse}/camel-k-client-${versionToUse}-${platformString}-64bit.tar.gz`;
-	const kamelCliFile = `camel-k-client-${versionToUse}-${platformString}-64bit.tar.gz`;
-	const downloadFile = path.join(installFolder, binFile);
+	const kamelUrl: string = `https://github.com/apache/camel-k/releases/download/${versionToUse}/camel-k-client-${versionToUse}-${platformString}-64bit.tar.gz`;
+	const kamelCliFile: string = `camel-k-client-${versionToUse}-${platformString}-64bit.tar.gz`;
+	const downloadFile: string = path.join(installFolder, binFile);
 
-	await versionUtils.pingGithubUrl(kamelUrl).then( (result) => {
-		if (!result) {
-			var msg = `Camel K CLI Version ${versionToUse} unavailable. Please check the Apache Camel K version specified in VS Code Settings. Inaccessible url: ${kamelUrl}`;
-			extension.shareMessageInMainOutputChannel(msg);
-			throw new Error(msg);
-		}
-	});
+	const result: boolean = await versionUtils.pingGithubUrl(kamelUrl);
+	if (!result) {
+		var msg = `Camel K CLI Version ${versionToUse} unavailable. Please check the Apache Camel K version specified in VS Code Settings. Inaccessible url: ${kamelUrl}`;
+		extension.shareMessageInMainOutputChannel(msg);
+		throw new Error(msg);
+	}
 
 	extension.shareMessageInMainOutputChannel(`Downloading kamel cli tool from ${kamelUrl} to ${downloadFile}`);
 
-	await downloadAndExtract(kamelUrl, kamelCliFile, installFolder, true)
-	.then( async (flag) => {
+	try { 
+		const flag = await downloadAndExtract(kamelUrl, kamelCliFile, installFolder, true);
 		console.log(`Downloaded ${downloadFile} successfully: ${flag}`);
 		if (fs.existsSync(downloadFile)) {
 			if (shell.isUnix()) {
@@ -178,11 +138,10 @@ export async function installKamel(context: vscode.ExtensionContext): Promise<Er
 			}
 	 		await config.addKamelPathToConfig(downloadFile);
 		}
-	})
-	.catch ( (error) => {
+	} catch( error ) {
 		console.log(error);
 		return { succeeded: false, error: [`Failed to download kamel: ${error}`] };
-	});
+	}
 
 	return { succeeded: true, result: null };
 }
@@ -192,60 +151,49 @@ function getInstallFolder(tool: string, context : vscode.ExtensionContext): stri
 }
 
 async function getStableKubectlVersion(): Promise<Errorable<string>> {
-	const downloadResult = await downloader.toTempFile('https://storage.googleapis.com/kubernetes-release/release/stable.txt');
+	const downloadResult: Errorable<string> = await downloader.toTempFile('https://storage.googleapis.com/kubernetes-release/release/stable.txt');
 	if (failed(downloadResult)) {
 		return { succeeded: false, error: [`Failed to establish kubectl stable version: ${downloadResult.error[0]}`] };
 	}
-	const version = fs.readFileSync(downloadResult.result, 'utf-8');
+	const version: string = fs.readFileSync(downloadResult.result, 'utf-8');
 	fs.unlinkSync(downloadResult.result);
 	return { succeeded: true, result: version };
 }
 
-function concatIfBoth(s1: string | undefined, s2: string | undefined): string | undefined {
-	return s1 && s2 ? s1.concat(s2) : undefined;
-}
-
-function home(): string {
-	return process.env['HOME'] ||
-		concatIfBoth(process.env['HOMEDRIVE'], process.env['HOMEPATH']) ||
-		process.env['USERPROFILE'] ||
-		'';
-}
-
 function getKubectlInstallFolder(tool: string): string {
-	return path.join(home(), `.vs-kubernetes/tools/${tool}`);
+	return path.join(shell.home(), `.vs-kubernetes/tools/${tool}`);
 }
 
 export async function installKubectl(context: vscode.ExtensionContext): Promise<Errorable<null>> {
-	const tool = 'kubectl';
-	const binFile = (shell.isUnix() || shell.isMacOS()) ? tool : `${tool}.exe`;
-	const os = shell.getPlatform();
-	let osString = os; // user looks for strings windows, darwin, linux
+	const tool: string = 'kubectl';
+	const executable: string = (shell.isUnix() || shell.isMacOS()) ? tool : `${tool}.exe`;
+	const os: string | undefined = shell.getPlatform();
+	let osString: string | undefined = os; // user looks for strings windows, darwin, linux
 	if (osString === 'win32') {
 		osString = 'windows';
 	}
 
-	const version = await getStableKubectlVersion();
+	const version: Errorable<string> = await getStableKubectlVersion();
 	if (failed(version)) {
 		return { succeeded: false, error: version.error };
 	}
 
-	const installFolder = getKubectlInstallFolder(tool);
+	const installFolder: string = getKubectlInstallFolder(tool);
 	console.log(`Downloading Kubernetes CLI to ${installFolder}`);
 	mkdirp.sync(installFolder);
 
-	const kubectlUrl = `https://storage.googleapis.com/kubernetes-release/release/${version.result.trim()}/bin/${osString}/amd64/${binFile}`;
-	const downloadFile = path.join(installFolder, binFile);
+	const kubectlUrl: string = `https://storage.googleapis.com/kubernetes-release/release/${version.result.trim()}/bin/${osString}/amd64/${executable}`;
+	const downloadFile: string = path.join(installFolder, executable);
 
 	extension.shareMessageInMainOutputChannel(`Downloading Kubernetes cli tool from ${kubectlUrl} to ${downloadFile}`);
 
-	await downloadAndExtract(kubectlUrl, binFile, installFolder, true)
-	.then( async (flag) => {
+	try { 
+		const flag: boolean = await downloadAndExtract(kubectlUrl, executable, installFolder, true);
 		console.log(`Downloaded ${downloadFile} successfully: ${flag}`);
-	}).catch ( (error) => {
+	} catch (error) {
 		console.log(error);
 		return { succeeded: false, error: [`Failed to download kubectl: ${error}`] };
-	});
+	}
 	await config.addKubectlPathToConfig(downloadFile);
 
 	if (shell.isUnix() || shell.isMacOS()) {
