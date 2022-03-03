5f721f7183d829d2363fd0fcbc9293397ae5dc5e
Aurélien Pupier <apupier@redhat.com>
2022-03-03 11:43:02 +0100

    FUSETOOLS2-1456 - upgrade default Camel K to 1.8.1
    note that previous regression to deploy several resources has
    unfortunately not been fixed in Camel K 1.8.1 despite it was marked as
    such. Opened a new issue https://github.com/apache/camel-k/issues/3077
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/suite/versionUtils.test.ts
+++ src/test/suite/versionUtils.test.ts
@@ -78,13 +78,19 @@ suite("VersionUtils check", () => {
 			await validateVersion('1.8.0', Platform.LINUX, 'https://github.com/apache/camel-k/releases/download/v1.8.0/camel-k-client-1.8.0-linux-64bit.tar.gz');
 		});
 		
-		test("validate url for existing 1.8.0 windows version", async () => {
-			await validateVersion('1.8.0', Platform.WINDOWS, 'https://github.com/apache/camel-k/releases/download/v1.8.0/camel-k-client-1.8.0-windows-64bit.tar.gz');
+		test("validate url for existing 1.8.1 version", async () => {
+			await validateVersion('1.8.1', Platform.LINUX, 'https://github.com/apache/camel-k/releases/download/v1.8.1/camel-k-client-1.8.1-linux-64bit.tar.gz');
+		});
+		
+		test("validate url for existing 1.8.1 windows version", async () => {
+			await validateVersion('1.8.1', Platform.WINDOWS, 'https://github.com/apache/camel-k/releases/download/v1.8.1/camel-k-client-1.8.1-windows-64bit.tar.gz');
 		});
 
-		test("validate url for existing 1.8.0 MacOS version", async () => {
-			await validateVersion('1.8.0', Platform.MACOS, 'https://github.com/apache/camel-k/releases/download/v1.8.0/camel-k-client-1.8.0-mac-64bit.tar.gz');
+		test("validate url for existing 1.8.1 MacOS version", async () => {
+			await validateVersion('1.8.1', Platform.MACOS, 'https://github.com/apache/camel-k/releases/download/v1.8.1/camel-k-client-1.8.1-mac-64bit.tar.gz');
 		});
+		
+		
 
 		test("validate invalid url for xyz1 version", async () => {
 			await invalidateVersion('xyz1', Platform.LINUX);
