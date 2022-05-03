0a4dd97ee7a98e1cc355cd38024ab65f126c7c53
Dominik Jelinek <djelinek@redhat.com>
2022-05-03 20:01:29 +0200

    Add UI test for camelk cli download
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/ui-test/camelk_extension_test.ts
+++ src/ui-test/camelk_extension_test.ts
@@ -1,11 +1,15 @@
 import * as pjson from '../../package.json';
 import { assert } from 'chai';
 import {
+	By,
 	EditorView,
-	ExtensionsViewItem
+	ExtensionsViewItem,
+	until,
+	VSBrowser,
+	WebDriver
 } from 'vscode-extension-tester';
 import {
-	Marketplace
+	Marketplace, StatusBarExt
 } from 'vscode-uitests-tooling';
 
 describe('Language Support for Apache Camel extension', function () {
@@ -14,15 +18,30 @@ describe('Language Support for Apache Camel extension', function () {
 	describe('Extensions view', function () {
 		let marketplace: Marketplace;
 		let item: ExtensionsViewItem;
+		let driver: WebDriver;
 
 		before(async function () {
 			this.retries(5);
+			driver = VSBrowser.instance.driver;
 			marketplace = await Marketplace.open(this.timeout());
 		});
 
 		after(async function () {
 			await marketplace.close();
 			await new EditorView().closeAllEditors();
+		});
+
+		it('Downloading of Apache Camel K CLI', async function () {
+			const statusbar = await driver.wait(until.elementLocated(By.id('redhat.vscode-camelk')), 35000);
+			await driver.wait(async () => {
+				const text = await statusbar.getText().catch(() => '');
+				try {
+					return text.startsWith('Download progress: ') && text.includes('100%');
+				}
+				catch {
+					return false;
+				}
+			}, this.timeout() - 3000, `Could not find Apache Camel K element with label "Download progress: ...". Current label: "${await new StatusBarExt().getItemByID('redhat.vscode-camelk').catch(() => 'unknown')}"`);
 		});
 
 		it('Find extension', async function () {
