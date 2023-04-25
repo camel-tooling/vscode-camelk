6f179f9b40fbe49ec651ca5463dd8b6ad75fb9fb
Mikhail Abramov <mabramov@redhat.com>
2023-04-25 16:26:06 +0200

    Update test runner

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -3,21 +3,20 @@ import * as cp from 'child_process';
 
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from '@vscode/test-electron';
 
-async function main() : Promise<void> {
+async function main(): Promise<void> {
 	try {
-		const extensionDevelopmentPath : string = path.resolve(__dirname, '../../../');
+		const extensionDevelopmentPath: string = path.resolve(__dirname, '../../../');
 		console.log(`extensionDevelopmentPath = ${extensionDevelopmentPath}`);
-        const extensionTestsPath : string = path.resolve(__dirname, './suite/index');
+		const extensionTestsPath: string = path.resolve(__dirname, './suite/index');
 		console.log(`extensionTestsPath = ${extensionTestsPath}`);
-		let vscodeExecutablePath : string;
-		const vscodeVersionForTest = process.env.VSCODE_VERSION_TEST;
-		if(vscodeVersionForTest !== undefined){
-			vscodeExecutablePath = await downloadAndUnzipVSCode(vscodeVersionForTest);
-		} else {
-			vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
-		}
-		console.log(`vscodeExecutablePath = ${vscodeExecutablePath}`);
 		const testWorkspace = path.resolve(__dirname, '../../../test Fixture with speci@l chars');
+		console.log(`testWorkspace = ${testWorkspace}`);
+
+		const vscodeVersion = computeVSCodeVersionToPlayTestWith();
+		console.log(`vscodeVersion = ${vscodeVersion}`);
+
+		const vscodeExecutablePath: string = await downloadAndUnzipVSCode(vscodeVersion);
+		console.log(`vscodeExecutablePath = ${vscodeExecutablePath}`);
 
 		const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 		installExtraExtension(cliPath, 'ms-kubernetes-tools.vscode-kubernetes-tools', args);
@@ -27,9 +26,20 @@ async function main() : Promise<void> {
 		await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace, '--disable-workspace-trust'] });
 
 	} catch (err) {
-		console.error('Failed to run tests' + err);
+		console.error('Failed to run tests: ' + err);
 		process.exit(1);
 	}
+
+	function computeVSCodeVersionToPlayTestWith() {
+		const envVersion = process.env.CODE_VERSION;
+		if (envVersion === undefined || envVersion === 'max') {
+			return 'stable';
+		} else if (envVersion === 'latest') {
+			return 'insiders';
+		}
+		return envVersion;
+	}
+
 }
 
 function installExtraExtension(cliPath: string, extensionId: string, args: string[]) {
