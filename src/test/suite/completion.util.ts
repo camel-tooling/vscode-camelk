f911304bc1dcc0ce38d538bed393855bfca13b5b
Aurélien Pupier <apupier@redhat.com>
2024-05-06 10:50:47 +0200

    modify condition to try to stay compatible with other tests and previous version

--- src/test/suite/completion.util.ts
+++ src/test/suite/completion.util.ts
@@ -37,7 +37,8 @@ export async function checkExpectedCompletion(docUri: vscode.Uri, position: vsco
 				console.log(`expectedCompleItemLabel: ${(expectedCompletion.label as vscode.CompletionItemLabel).label}`);
 				const completionItemFound = actualCompletionList.items.find(completion => {
 					console.log(`compleitemLabel: ${completion.label.toString()}`);
-					if(completion.label.toString() === (expectedCompletion.label as vscode.CompletionItemLabel).label) {
+					if(completion.label.toString() === (expectedCompletion.label as vscode.CompletionItemLabel).label
+						|| completion.label.toString() === expectedCompletion.label.toString()) {
 						console.log('same label found');
 						if(completion.documentation === expectedCompletion.documentation) {
 							console.log('same documentation found');
